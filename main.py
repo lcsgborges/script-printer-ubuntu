ee00cdd1a9b474e361f72ef958792536cf6e45bf
lcsgborges <lcsgborges@gmail.com>
2025-10-24 11:29:46 -0300

    feat: script printer

---
+++ main.py
@@ -0,0 +1,20 @@
+from dotenv import load_dotenv
+import os
+import subprocess
+
+load_dotenv()
+
+USER = os.getenv('USER')
+PRINTER = os.getenv('PRINTER')
+
+END_FILE = '.pdf'
+
+FOLDER_PATH = f"/home/{USER}/Pessoal/Certificados/"
+
+for filename in os.listdir(FOLDER_PATH):
+    if filename.endswith(END_FILE):
+        filepath = os.path.join(FOLDER_PATH, filename)
+        
+        print('Print file:', filename)
+        
+        subprocess.run(["-lp", "-d", PRINTER, filepath])
